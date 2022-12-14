1b174e0300c8347c078cb318081460fe4d13b21f
MinatoSuzuki <111655867+MinatoSuzukidev@users.noreply.github.com>
2022-12-14 19:12:25 +0900

    Initial commit

---
+++ typings.d.ts
@@ -0,0 +1,45 @@
+export interface Genre {
+    id: number
+    name: string
+  }
+
+export interface Movie {
+    title: string
+    backdrop_path: string
+    media_type?: string
+    release_date?: string
+    first_air_date: string
+    genre_ids: number[]
+    id: number
+    name: string
+    origin_country: string[]
+    original_language: string
+    original_name: string
+    overview: string
+    popularity: number
+    poster_path: string
+    vote_average: number
+    vote_count: number
+  }
+
+export interface Element {
+    type:
+      | 'Bloopers'
+      | 'Featurette'
+      | 'Behind the Scenes'
+      | 'Clip'
+      | 'Trailer'
+      | 'Teaser'
+  }
+
+export interface Product {
+    description: string
+    name: string
+    role: string
+    resolution: string
+    number_of_devices: number
+    id: number
+    videoQuality: string
+    price: string
+    portability:boolean
+  }
